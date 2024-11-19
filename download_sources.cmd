cf40c318c13b99ea50c3553666e8074892d6ca26
offhub <6871698+offhub@users.noreply.github.com>
2024-11-20 02:16:43 +0300

    Update download_sources.cmd

--- download_sources.cmd
+++ download_sources.cmd
@@ -27,10 +27,9 @@ REM Downloading Qt 6.8 source code...
 echo Downloading Qt %qt_version% source code...
 curl -LsSO --output-dir "%bDir%" "%qt_source_url%"
 dir
-ren "qt-everywhere-src-%qt_version%.zip" "qt-everywhere-opensource-src-%qt_version%.zip"
-dir
 "C:\Program Files\7-Zip\7z.exe" x -aoa -o"%bDir%" "%bDir%\qt-everywhere-opensource-src-%qt_version%.zip"
 dir
+ren "%bDir%\qt-everywhere-src-%qt_version%" "qt-everywhere-opensource-src-%qt_version%"
 move "%bDir%\qt-everywhere-opensource-src-%qt_version%" "%bDir%\src"
 dir
 
